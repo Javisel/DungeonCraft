43a4fda8a069383158494f8d6c103806711b55d7
Javisel <36491374+Javisel@users.noreply.github.com>
2020-02-29 00:50:57 -0500

    Healthbar bugfixing

--- src/main/java/com/dungeoncraftteam/dungeoncraft/client/OverlayHandler.java
+++ src/main/java/com/dungeoncraftteam/dungeoncraft/client/OverlayHandler.java
@@ -62,7 +62,7 @@ public class OverlayHandler extends GuiUtils {
     public void renderHealthBar() {
         MainWindow scaledresolution = instance.mainWindow;
 
-        int x = scaledresolution.getScaledHeight();
+        int x = scaledresolution.getScaledWidth();
 
         int y = scaledresolution.getScaledHeight();
 
@@ -70,7 +70,7 @@ public class OverlayHandler extends GuiUtils {
 
 
         //Healthbar
-        drawTexturedModalRect((x/2)+91,y-39,2,53,barwidth,barheight,-1);
+        drawTexturedModalRect((x/2)-92,y-40,2,53,barwidth,barheight,-1);
         float healthratio = instance.player.getHealth()/instance.player.getMaxHealth();
 
         GlStateManager.pushMatrix();
@@ -89,7 +89,13 @@ public class OverlayHandler extends GuiUtils {
 
 
         }
-        drawTexturedModalRect(((x/2)+92),y-39+1,3,75, (int) (81*healthratio),8,-1);
+
+        drawTexturedModalRect(((x/2)-91),y-40+1,3,75, (int) (81*healthratio),8,-1);
+
+        GlStateManager.color3f(0,0,0);
+
+        String hp = instance.player.getHealth() + "/" +instance.player.getMaxHealth();
+        instance.fontRenderer.drawString(hp,((x/2)-91)+instance.fontRenderer.getStringWidth(hp)/4,y-39,Color.WHITE.getRGB()   );
 
         GlStateManager.popMatrix();
 
@@ -104,7 +110,7 @@ public class OverlayHandler extends GuiUtils {
     public void renderActives(){
         MainWindow scaledresolution = instance.mainWindow;
 
-        int x = scaledresolution.getScaledHeight();
+        int x = scaledresolution.getScaledWidth();
 
         int y = scaledresolution.getScaledHeight();
 
